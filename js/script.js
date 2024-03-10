b007353e36d347da2ea7e5edd8c0a90c653302c9
Tuhina16 <tuhinamajumder16@gmail.com>
2024-03-10 22:44:47 +0530

    new file:   css/style.css 	new file:   images/TM.png 	new file:   images/Tuhina Majumder.jpeg 	new file:   images/Untitled design.png 	new file:   images/wp1.jpeg 	new file:   images/wp2.jpeg 	new file:   index.html 	new file:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function {
+    const navbar = document.querySelector("navbar-nav ml-auto");
+    const icon = document.querySelector("collapse navbar-collapse justify-content-end");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
